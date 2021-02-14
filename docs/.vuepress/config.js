2b3533b8322de9e7fcfa5219da30ba77bfbde3cd
sincerexie <sincerexie_dev@hotmail.com>
2021-02-15 00:27:22 +0800

    update doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  title: 'Books written by Sincere Xie',
-  description: 'Just enjoy it.'
+  title: 'Books',
+  description: 'Just enjoy it.',
+  themeConfig: {
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'sincerexie/tech-books',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: 'Github',
+  }
 }
---
+++ docs/index.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: home_icon.png
+heroText: Books
+tagline: 🚀 Continuous learning of development techniques.
+sidebar: false
+search: false
+features:
+- title: 
+  details: 
+footer: MIT Licensed | Copyright © 2021-present Sincere Xie
+---
+
+# 📖 Books list
+
++ 🔥  [『庖丁解牛』学babel](./learn-babel/)
