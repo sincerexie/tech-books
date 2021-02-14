966e46bef0ba93c085176b7a52af4333ad6fad98
sincerexie <sincerexie_dev@hotmail.com>
2021-02-14 17:14:19 +0800

    modify deloy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env sh
 
-# 部署脚本
-
 # 确保脚本抛出遇到的错误
 set -e
 
 # 生成静态文件
 npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 发布到自定义域名
+echo 'books.sincerexie.com' > CNAME
 
 # 创建gh-pages，并部署
 npm run deploy
